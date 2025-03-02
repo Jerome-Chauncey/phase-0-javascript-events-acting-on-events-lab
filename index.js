e75b412a49343ec5bff1992669d255ec73597a1f
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-02 15:03:47 +0300

    makes dodger move left and right

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+// const { parse } = require("@babel/core");
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "orange";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left <360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
